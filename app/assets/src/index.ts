57e0ff3a6894b845d7f6ad5a9205a991bd35db13
Александр <administrator@MacBook-Pro-Aleksandr-2.local>
2024-05-24 00:12:00 +0300

    sass/scss support

--- app/assets/src/index.ts
+++ app/assets/src/index.ts
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './components/App'
 
+import './styles/main.scss'
+
 
 const container = document.getElementById('root') as Element
 const root = createRoot(container)
